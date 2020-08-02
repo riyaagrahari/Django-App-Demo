9179555da5e2fc056215484ad1f2374a7153971e
riyaagrahari <riyaagrahari121@gmail.com>
2020-08-02 13:03:47 +0530

    Context:variable, for, if-else

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 
-def News(request):
-    return render(request, 'news.html')
 
 def Home(request):
-    return render(request, 'home.html')
+
+    context = {
+        "name":"Riya Agrahari",
+        "number": 75792
+    }
+
+    return render(request, 'home.html', context)
+
+
+def News(request):
+
+    context = {
+        "list":["Python", "Java", "C++", "C#", "Kotlin", "Ruby"],
+        "mynum": 40
+    }
+    return render(request, 'news.html', context)
+
+   
+
 
 def Contact(request):
     return render(request, 'contact.html')
